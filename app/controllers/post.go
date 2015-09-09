8d1b9b93fca96d13675512870f683b99ba97755e
4pcbr <me@4pcbr.com>
2015-09-09 09:08:27 +0200

    Implemented a post listing template

--- app/controllers/post.go
+++ app/controllers/post.go
@@ -3,13 +3,21 @@ package controllers
 import (
     // "blog/app/models"
     "github.com/revel/revel"
+    "blog/app/models"
 )
 
 type PostController struct {
-    GorpController
+    GormController
+}
+
+func ( c PostController ) List() revel.Result {
+    var posts []models.Post
+    Db.Where( "published = 1" ).Order( "created_at desc" ).Limit(5).Find(&posts)
+    return c.Render(posts) // TODO
 }
 
 func ( c PostController ) Show ( post_token string ) ( revel.Result ) {
+
     //post := models.Post{}
     //Txn.Where( "token = ?", post_token ).First( &post )
     return c.Render( nil )
